bd38a58f915a20c0e1aa391dfe92994e615a4c35
Ben Lu <ben@ben-l.com>
2016-07-31 16:22:31 +1200

    forgot files

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+// Cube
+// -----
+qget('figure.front').onclick = function(){
+  console.log('run!');
+  qget('#cube').className = 'show-top';
+}
+qget('figure.top').onclick = function(){
+  qget('#cube').className = 'show-right';
+}
+qget('figure.right').onclick = function(){
+  qget('#cube').className = 'show-back';
+}
+qget('figure.back').onclick = function(){
+  qget('#cube').className = 'show-bottom';
+}
+qget('figure.bottom').onclick = function(){
+  qget('#cube').className = 'show-left';
+}
+qget('figure.left').onclick = function(){
+  qget('#cube').className = 'show-front';
+}
